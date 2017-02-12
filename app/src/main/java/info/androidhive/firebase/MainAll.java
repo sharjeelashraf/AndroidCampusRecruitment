852dd3ea2a28830eecbe92138716a278a86c628e
operation badar <ob@gmail.com>
2017-02-12 00:03:36 -0800

    firt

---
+++ app/src/main/java/info/androidhive/firebase/MainAll.java
@@ -0,0 +1,49 @@
+package info.androidhive.firebase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainAll extends AppCompatActivity {
+
+Button button, button1, button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all_main);
+
+        addListenerOnButton();
+
+    }
+
+    public void addListenerOnButton() {
+
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(MainAll.this, LoginActivity.class);
+                startActivity(i);
+
+            }
+
+        });
+
+    }
+}
